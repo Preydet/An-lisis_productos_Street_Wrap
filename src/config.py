480c3418fe807680652207571eb4e1f259648eaf
Preydet <pareydet@gmail.com>
2026-08-28 12:53:50 -0400

    feat: agrega index.html para despliegue automatico en GitHub Pages

--- src/config.py
+++ src/config.py
@@ -4,17 +4,17 @@ Centraliza rutas y parámetros del análisis
 """
 from pathlib import Path
 
-# Ruta raiz del proyecto 
+# Ruta raíz del proyecto 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#Carpetas principales
+# Carpetas principales
 DATA_DIR = BASE_DIR / "data"
 OUTPUT_DIR = BASE_DIR / "output"
 
-#Crear carpetas automática si no existen
+# Crear carpetas automáticamente si no existen
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 # Rutas de archivos por defecto
 DEFAULT_CSV_PATH = DATA_DIR / "cantidad_productos.csv"
-OUTPUT_HTML_REPORT = OUTPUT_DIR / "reporte_interactivo_ventas.html"
+OUTPUT_HTML_REPORT = BASE_DIR / "index.html"  # Guardado en la raíz para despliegue directo
